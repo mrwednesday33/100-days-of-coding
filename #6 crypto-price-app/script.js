a4ca854d9f32e74a6746ab2dc637199d60ca003d
Clement Lai <104053429+mrwednesday33@users.noreply.github.com>
2023-11-03 15:09:07 +0800

    Add files via upload

---
+++ #6 crypto-price-app/script.js
@@ -0,0 +1,65 @@
+const canvasContainer = document.querySelector('.canvas-container');
+const canvas = document.createElement('canvas');
+canvasContainer.appendChild(canvas);
+
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const particles = [];
+
+class Particle {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.size = Math.random() * 5 + 1;
+        this.speedX = Math.random() * 3 - 1.5;
+        this.speedY = Math.random() * 3 - 1.5;
+    }
+
+    update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+
+        if (this.size > 0.2) this.size -= 0.1;
+    }
+
+    draw() {
+        ctx.fillStyle = '#ffffff';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.closePath();
+        ctx.fill();
+    }
+}
+
+function createParticles(x, y) {
+    const particleCount = 5;
+    for (let i = 0; i < particleCount; i++) {
+        particles.push(new Particle(x, y));
+    }
+}
+
+function animateParticles() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let i = 0; i < particles.length; i++) {
+        particles[i].update();
+        particles[i].draw();
+
+        if (particles[i].size <= 0.2) {
+            particles.splice(i, 1);
+            i--;
+        }
+    }
+
+    requestAnimationFrame(animateParticles);
+}
+
+canvas.addEventListener('mousemove', (event) => {
+    const { clientX, clientY } = event;
+    createParticles(clientX, clientY);
+});
+
+animateParticles();
